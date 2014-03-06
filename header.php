7bf4552e37640ac3f10d45e1f00e01d69902a803
luis <luis19prz@gmail.com>
2014-03-06 10:48:19 -0600

    header

---
+++ header.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Gamecentral</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<div id="wrap">
+		<div id="topBackRepeat">
+			
+			<div id="main">
+				<header>
+					<div class="logobar">
+						<a href="/"><img src="img/Logo.png" alt="GameCentral"></a>
+					</div>
+					<nav>
+						<span class="blacksquare"></span>
+						<a href="#">Inicio</a>
+						<a href="#">Reseñas</a>
+						<a href="#">Catalogo</a>
+						<a href="#">Sucursales</a>
+						<a href="#">Contacto</a>
+					</nav>
+				</header>
+				<aside></aside>
+
+				<div id="content">
+					
