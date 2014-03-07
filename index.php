93389e68a612d33d1be4573e284e0c9e875a934b
luis <luis19prz@gmail.com>
2014-03-06 22:13:25 -0600

    imagen del post

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php include("header.php"); ?>
 
- 	<div class="container singlepost">
- 		<h2>Nuestra recomendacion</h2>
- 		<h1>Grand Theft Auto V</h1>
- 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quisquam, perspiciatis, soluta, totam voluptas dolorem illum laborum ipsum nam iusto neque rem quibusdam unde harum optio natus praesentium repellendus voluptates.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, dignissimos, animi, eos consectetur tempora rem culpa nisi laudantium dolor odit assumenda est a itaque laboriosam iusto natus eum. Debitis, fugiat.</p>
- 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, earum, aspernatur, modi placeat corporis dolorem voluptatibus cupiditate nostrum impedit quae veniam vitae est soluta suscipit velit dolores rem esse ad?</p>
-		<div class="videobox"><div class="video_container"><iframe  src="//www.youtube.com/embed/N-xHcvug3WI" frameborder="0" allowfullscreen></iframe></div></div>
+ 	<div class="container">
+ 		<div class="imagepost"><div class="overlay"></div><img src="img/gta.jpg" alt="Grand Theft Auto V"></div>
+		<section class="singlepost">
+	 		<div class="postTitle">
+		 		<h2>Nuestra recomendacion</h2>
+		 		<h1>Grand Theft Auto V</h1>
+	 		</div>
+	 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, quisquam, perspiciatis, soluta, totam voluptas dolorem illum laborum ipsum nam iusto neque rem quibusdam unde harum optio natus praesentium repellendus voluptates.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, dignissimos, animi, eos consectetur tempora rem culpa nisi laudantium dolor odit assumenda est a itaque laboriosam iusto natus eum. Debitis, fugiat.</p>
+	 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, earum, aspernatur, modi placeat corporis dolorem voluptatibus cupiditate nostrum impedit quae veniam vitae est soluta suscipit velit dolores rem esse ad?</p>
+			<div class="videobox"><div class="video_container"><iframe  src="//www.youtube.com/embed/N-xHcvug3WI" frameborder="0" allowfullscreen></iframe></div></div>
+		</section>
 	</div>
 
 <?php include("footer.php"); ?>
