ff4d5779d6a2950dcd61b9986118c56f143a22d9
EzeroVivanco <zero.vivanco@gmail.com>
2014-04-06 12:20:39 -0500

    noticia 2

---
+++ noticias.php
@@ -0,0 +1,44 @@
+<?php include("header.php");?>
+	<div class="container">
+		<div class="news card">
+			<div class="imagenews"><img src="img/gmc.png" alt="GC" width= 180 height=110></div>
+			<div class="text">
+				<h2>El estelar del mes</h2>
+				<h3>01-Abril-2014 | Autor: Redactor</h3>
+				<p>Conoce nuestro novedoso sistema de trabajo y forma parte de la industria de los video juegos. Si deseas saber cómo, te recomendamos leer nuestra nota completa.</p>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="news card">
+			<div class="imagenews"><img src="img/gtavbg.png" alt="GTA V" width= 180 height=110></div>
+			<div class="text">
+				<h2>La recomendación: Grand Theft Auto V</h2>
+				<h3>02-Abril-2014 | Autor: Redactor</h3>
+				<p>!La espera termino! Ya podemos disfrutar de la nueva entraga de Rock Star,</p>
+				<p>sin lugar a dudas Grand Theft Auto V es el título más ambicioso de los</p>
+				<p>últimos 7 años, la calidad gráfica, variedad de juego y...</p>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="news card">
+			<div class="imagenews"><img src="img/titan.jpg" alt="Titan" width= 180 height=110></div>
+			<div class="text">
+				<h2>La Xbox One con diseño de Titanfall no se comercializará.</h2>
+				<h3>03-Abril-2014 | Autor: Redactor</h3>
+				<p>La Xbox One con diseño de Titanfall que un supuesto trabajador de Microsoft filtró, finalmente no se comercializara. Resulta que...</p>
+				<br>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="news card">
+			<div class="imagenews"><img src="img/Castlevania.jpg" alt="Castlevania" width= 180 height=110></div>
+			<div class="text">
+				<h2>Se viene primer DLC de Castlevania: Lords of Shadow 2.</h2>
+				<h3>04-Abril-2014 | Autor: Redactor</h3>
+				<p>Konami confirma el primer DLC donde podremos jugar con Alucard en este primer contenido descargable de Castlevania: Lords of Shadow 2. Este DLC,sera llamado "Revelations", se pondrá a la venta el...</p>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="clear"></div>
+	</div>
+<?php include("footer.php");?>
